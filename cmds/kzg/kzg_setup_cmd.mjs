76add2ae0213337e4b35233a6839ea96c3a24542
Xavi Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2022-10-19 09:35:27 +0200

    add kzg setup

--- cmds/kzg/kzg_setup_cmd.mjs
+++ cmds/kzg/kzg_setup_cmd.mjs
@@ -1,4 +1,5 @@
 import {kzgSetup} from "../../src/services/kzg_services.js";
+import {resolve} from "path";
 
 export const command = 'setup <pilFilename> <cnstPolsFilename> <ptauFilename> <preprocessedFilename>'
 export const desc = '...' //TODO
@@ -22,6 +23,12 @@ export const builder = (yargs) => yargs
     });
 export const handler = async function (argv) {
     const options = {verbose: argv.verbose || false};
+
+    argv.pilFilename = resolve(argv.pilFilename);
+    if (undefined !== argv.pilConfigFilename) argv.pilConfigFilename = resolve(argv.pilConfigFilename);
+    argv.cnstPolsFilename = resolve(argv.cnstPolsFilename);
+    argv.ptauFilename = resolve(argv.ptauFilename);
+    argv.preprocessedFilename = resolve(argv.preprocessedFilename);
 
     await kzgSetup(argv.pilFilename, argv.pilConfigFilename, argv.cnstPolsFilename, argv.ptauFilename, argv.preprocessedFilename, options);
 }
